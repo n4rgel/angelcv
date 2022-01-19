c4987a59f919b24c55ded3a073c415ec5d1d6bd6
n4rgel <31897570+n4rgel@users.noreply.github.com>
2022-01-19 16:25:03 +0100

    first steps for read data from a json

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import * as datajson from './cv.json';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
-
+  data: any;
+  constructor(private router: Router) {}
+  ngOnInit() {
+    console.log(datajson);
+    this.data = datajson;
+  }
+  LogIn(){
+    this.router.navigate(['/login']);
+  }
 }
