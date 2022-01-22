d997aed857ff41f03ae6f53296742c55856fd617
n4rgel <31897570+n4rgel@users.noreply.github.com>
2022-01-22 15:20:01 +0100

    session control fixed

--- src/app/services/allring.service.ts
+++ src/app/services/allring.service.ts
@@ -1,29 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AllringService {
+export class AllringService implements OnInit, OnDestroy{
 
-  user:any=null;
+  user:any;
   constructor(public ngFireAuth: AngularFireAuth) { 
-    this.user=null;
+    
   }
 
   ngOnInit() {
-    
+    this.user=localStorage.getItem('user')
   }
+  ngOnDestroy() {
+  }
+  
   setloged(user){
       this.user=user;
+      localStorage.setItem('user',JSON.stringify(this.user));
     
   }
    isloged(){
-    //return this.ngFireAuth.user!=null;
+     
+    this.user=JSON.parse(localStorage.getItem('user'))
     return this.user!=null
     
   }
    getUser(){
+     
+    this.user=JSON.parse(localStorage.getItem('user'))
+    console.log(this.user)
     return this.user;
   }
   havePermission(){}
